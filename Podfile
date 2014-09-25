5494b256ccfde0d31608ce99d786516279d12cb7
John Hobbs <john@velvetcache.org>
2014-09-25 18:30:19 -0500

    UI and basic objective-c runner.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "QuickCode" do
+pod "ACEView"
+
+end
+
+target "QuickCodeTests" do
+
+end
+
--- QuickCode/QCAppDelegate.h
+++ QuickCode/QCAppDelegate.h
@@ -7,9 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <ACEView/ACEView.h>
 
 @interface QCAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet ACEView *input;
+@property (unsafe_unretained) IBOutlet NSTextView *output;
+
+@property (weak) IBOutlet NSButton *runButton;
+@property (weak) IBOutlet NSProgressIndicator *workingSpinner;
+@property (weak) IBOutlet NSComboBox *languageSelect;
+
+- (IBAction)runProgram:(id)sender;
 
 @end
